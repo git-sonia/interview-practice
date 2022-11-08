638220a2933ced14b0b859f3d4a54f5c9b10e449
git-sonia <111617493+git-sonia@users.noreply.github.com>
2022-11-08 14:34:14 -0800

    Create MergeSorted.java

---
+++ MergeSorted.java
@@ -0,0 +1,11 @@
+public class MergeSorted{
+   // Tip: You may use some of the code templates provided
+   // in the support file
+   public static int[] mergeSorted(int[] nums1, int m, int[] nums2, int n) {
+      
+      // Write your code here
+      
+      int[] list = new int[]{};
+      return list;
+   }
+}
