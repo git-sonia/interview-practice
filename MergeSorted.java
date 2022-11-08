b5d0b787ae98e5212c423ec70aa3360d47038714
git-sonia <111617493+git-sonia@users.noreply.github.com>
2022-11-08 14:44:11 -0800

    Update MergeSorted.java
    Consider end cases before coding

--- MergeSorted.java
+++ MergeSorted.java
@@ -1,11 +1,23 @@
-public class MergeSorted{
-   // Tip: You may use some of the code templates provided
-   // in the support file
+public class MergeSorted{ //merge nums2 into nums1 at the end, both are sorted
    public static int[] mergeSorted(int[] nums1, int m, int[] nums2, int n) {
-      
-      // Write your code here
-      
-      int[] list = new int[]{};
-      return list;
+      int p1 = m - 1;
+      int p2 = n - 1;
+      int p = m + n - 1;
+      while (p1 >= 0 && p2 >= 0) {
+         if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+         } else {
+            nums1[p] = nums2[p2];
+            p2--;
+         }
+         p--;
+      }
+      while (p2 >= 0) {
+         nums1[p] = nums2[p2];
+         p2--;
+         p--;
+      }
+      return nums1;
    }
 }
