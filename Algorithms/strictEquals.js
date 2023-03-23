ba16e42cc7ff74b19f0202213fd019811cce12bd
Sonia <tsonia5987@gmail.com>
2023-03-22 21:50:17 -0700

    strict equals test cases

--- Algorithms/strictEquals.js
+++ Algorithms/strictEquals.js
@@ -1,7 +1,36 @@
 let strictEquals = function(a, b) {
-  return true;
+  if (isNull(a) && isNull(b)) {
+    return false;
+  } else if (isValidNumberPair(a, b)) {
+    return true;
+  } else {
+    return Object.is(a, b);
+  }
 };
 
+let isNull = function(a) {
+  try {
+    if (typeof(a) === "object") {
+      a.length;
+    }
+    return false;
+  } catch {
+    return true;
+  }
+};
+
+let isValidNumberPair = function(a, b) {
+  if (typeof(a) === "number" && typeof(b) === "number") {
+    if (a - b === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
 console.log(strictEquals(1, 1) === true);
+console.log(strictEquals(1, 2) === false);
+console.log(strictEquals({}, {}) === false);
+console.log(strictEquals([1], [1]) === false);
 console.log(strictEquals(null, null) === false);
 console.log(strictEquals(0, -0) === true);
